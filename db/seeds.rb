3ad1ea20fdd588a0f175b82dfb97000c9ccd8cba
Eric Lacaille <elacaille0@gmail.com>
2016-05-11 12:39:19 +0200

    Models solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "01.42.98.45.35",
+    category: "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "01.42.26.18.52",
+    category: "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "01.47.08.40.71",
+    category: "japanese"
+  }
+]
+restaurants_attributes.each { |param| Restaurant.create!(param) }
